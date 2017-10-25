aa91540b300669e291cfc11891ea216a84bed2c7
2Chanz <JChan106@ucr.edu>
2017-10-24 22:39:08 -0700

    tried adding firebase

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import style from './App.scss';
 import SearchPage from './../SearchPage';
 import StartPage from './../StartPage';
-
+import * as firebase from 'firebase';
 
 /**
  * App Component
  * @type {Class}
  */
 export default class App extends React.Component {
+
+
   /**
    * Render function for App Component
    * @return {JSX} Component to render
