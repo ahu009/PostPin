f7aa59a55158bec9397b3be215f98fb16b54c290
Abe Hu <abe@Abes-MacBook-Pro.local>
2017-11-07 20:28:17 -0800

    Mapped category/tags to searchpageresults

--- src/components/SearchResultsPage/SearchResultsPage.js
+++ src/components/SearchResultsPage/SearchResultsPage.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import SearchBar from 'material-ui-search-bar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import style from './SearchResultsPage.scss';
 import Posting from './Posting';
+import auto from './../SearchPage/TempAutoFill';
 
 /**
  * UI Component
@@ -12,8 +16,28 @@ class SearchResultsPage extends React.Component {
    * @return {JSX} Component to render
    */
   render () {
+    let category = '';
+    if (sessionStorage.getItem("Category") != '') {
+      category = `Category: ${sessionStorage.getItem("Category")}`;
+    }
+
+    let placeHolderText = `Search for Pins in ${sessionStorage.getItem("schoolName")} -- ${category}`;
     return (
       <div className={style.container}>
+      <MuiThemeProvider>
+        <SearchBar
+          dataSource={auto}
+          onChange={() => console.log('onChange')}
+          onRequestSearch={() => this.toggleEnterClicked()}
+          placeholder={placeHolderText}
+          style={{
+            width: '55%',
+            position: 'absolute',
+            left: '23%',
+            top: '3%'
+          }}
+        />
+      </MuiThemeProvider>
         <div className={style.header}>
           <p> There are x results for {sessionStorage.getItem("userSearch")} </p>
         </div>
